e1d8d42ee6b8991051fdd84fc70b4408f3a6953f
JuvanValentic <urban.valentic2@gmail.com>
2024-06-11 11:19:56 +0200

    Add files via upload

---
+++ koda_urban_oskar.ino
@@ -0,0 +1,26 @@
+#include <LiquidCrystal.h>
+
+const int photoresistorPin = A0;//pin za foto resistor
+
+const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+const float calibrationFactor = 0.19; //kalibracija
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("Lux Meter Test");
+}
+
+void loop() {
+  int sensorValue = analogRead(photoresistorPin);
+
+  float lux = (1023 - sensorValue) * calibrationFactor;
+
+  lcd.setCursor(0, 1);
+  lcd.print("Lux: ");
+  lcd.print(lux);
+
+  delay(1000);
+}
